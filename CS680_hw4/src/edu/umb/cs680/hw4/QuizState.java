a99e818fac895973f8a72fbf6282619004819049
SyamSai23 <syamsaichippala23.com>
2023-10-26 22:17:34 -0400

    Remove folder-to-delete

--- CS680_hw4/src/edu/umb/cs680/hw4/QuizState.java
+++
@@ -1,7 +0,0 @@
-package edu.umb.cs680.hw4;
-
-public interface QuizState {
-//    creating an Abstract method by without define anything in the method
-    public void action(QuizAction QuizAction);
-}
-
