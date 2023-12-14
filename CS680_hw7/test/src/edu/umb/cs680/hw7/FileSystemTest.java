9d9221e352fda96617d03970daa83e75872edea1
SyamSai23 <syamsaichippala23.com>
2023-12-14 17:42:14 -0500

    Changes made

--- CS680_hw7/test/src/edu/umb/cs680/hw7/FileSystemTest.java
+++ CS680_hw7/test/src/edu/umb/cs680/hw7/FileSystemTest.java
@@ -2,14 +2,11 @@ package edu.umb.cs680.hw7;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import java.time.LocalDateTime;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class FileSystemTest {
     private static FileSystem fs;
-
     @BeforeAll
     public static void setUpFS(){
          fs= TestFixtureInitializer.CreateFS();
@@ -17,11 +14,11 @@ class FileSystemTest {
 
     @Test
     public void TestSingletonReturnsNOnNullValue(){
-        fs = FileSystem.getFileSystem();
-        assertNotNull(FileSystem.getFileSystem(), "NOT NULL");
+        FileSystem fs1 = FileSystem.getFileSystem();
+        FileSystem fs2 = FileSystem.getFileSystem();
+
+        assertSame(fs1, fs2, "Instance should be the same ");
     }
-
-
 
     @Test
     public void TeatAppendRootDir(){
@@ -29,6 +26,7 @@ class FileSystemTest {
         Directory d1 = new Directory(null,"repo",0, LocalDateTime.now());
         fs.appendRootDir(d1);
         assertTrue(fs.getRootDirs().contains(d1));
+        assertEquals(fs.hashCode(), fs.getFileSystem().hashCode());
     }
 
 
--- CS680_hw7/test/src/edu/umb/cs680/hw7/TestFixtureInitializer.java
+++ CS680_hw7/test/src/edu/umb/cs680/hw7/TestFixtureInitializer.java
@@ -6,29 +6,28 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 
 public class TestFixtureInitializer {
+    private static FileSystem fs;
+
     public static FileSystem CreateFS() {
-        FileSystem fs = FileSystem.getFileSystem();
+        if(fs == null){
+            fs = FileSystem.getFileSystem();
 
-        LocalDateTime date1 = LocalDateTime.of(2023,12,1,4,15);
-        LocalDateTime date2 = LocalDateTime.of(2023,12,2,11,30);
+            LocalDateTime date1 = LocalDateTime.of(2023,12,1,4,15);
+            LocalDateTime date2 = LocalDateTime.of(2023,12,2,11,30);
 
-        Directory d1 = new Directory(null,"repo",0,date1);
-        fs.appendRootDir(d1);
-        Directory d2 = new Directory(d1,"src",0,date1);
-        fs.getRootDirs().add(d2);
-        Directory d3 = new Directory(d1,"test",0,date1);
-        fs.getRootDirs().add(d3);
-        File f1 = new File(d1,"readme.md",3,date2);
-        File f2 = new File(d2,"A.java",4,date2);
-        File f3 = new File(d2,"B.java",5,date2);
-        Directory d4 = new Directory(d3,"src",0,date1);
-        File f4 = new File(d4,"ATest.java",5,date2);
-        File f5 = new File(d4, "BTest.java",7,date2);
-
-
-
-
-
+            Directory d1 = new Directory(null,"repo",0,date1);
+            fs.appendRootDir(d1);
+            Directory d2 = new Directory(d1,"src",0,date1);
+            fs.getRootDirs().add(d2);
+            Directory d3 = new Directory(d1,"test",0,date1);
+            fs.getRootDirs().add(d3);
+            File f1 = new File(d1,"readme.md",3,date2);
+            File f2 = new File(d2,"A.java",4,date2);
+            File f3 = new File(d2,"B.java",5,date2);
+            Directory d4 = new Directory(d3,"src",0,date1);
+            File f4 = new File(d4,"ATest.java",5,date2);
+            File f5 = new File(d4, "BTest.java",7,date2);
+        }
         return fs;
     }
 
